e054b0ad241c3eb425f98292090b3912a11a3296
Adrian Nalwalker <analwalker@planetfg.local>
2017-10-15 19:08:05 -0700

    time to update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,18 +9,45 @@ score: 0,
 initializeGame: function () {
 
 	var random = Math.floor(Math.random() * 101 +19);
+	var red = Math.floor(Math.random() * 12 +1);
+	var blue = Math.floor(Math.random() * 12 +1);
+	var yellow = Math.floor(Math.random() * 12 +1);
+	var green  = Math.floor(Math.random() * 12 +1);
 	var wins = 0;
 	var losses = 0;
 	var score = 0;
 	$('#random').html(random);
+	$('#red').data('gemValue', red);
+	$('#blue').data('gemValue', blue);
+	$('#yellow').data('gemValue', yellow);
+	$('#green').data('gemValue', green);
 	$('#wins').html("Wins:  " +wins);
 	$('#losses').html("Losses:  " +losses);
 	$('#score').html(score);
+	console.log($('#red').data('gemValue'));
+	console.log($('#blue').data('gemValue'));
+	console.log($('#yellow').data('gemValue'));
+	console.log($('#green').data('gemValue'));
+	
 
 },
 
+// guess: function() {
+// 	$('#score').html(this.data);
+
+// 	},
+
+
+
 
 };
-console.log("I'm here!")
+
 crystalGame.initializeGame();
+
+$('.gem').on('click', function() {
+
+	$(this).data('gemValue');
+	console.log(clickValue);
+	}
+);
 
